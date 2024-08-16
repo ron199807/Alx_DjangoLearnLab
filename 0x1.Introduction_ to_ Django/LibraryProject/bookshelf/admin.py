f5c8f91747952f5d8ef087e36deceb63595379ad
ron199807 <ronaldmweema@gmail.com>
2024-08-16 02:09:54 +0200

    added a relations_app

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from .models import Book
 
-admin.site.register(Book)
-
 # customizing the admin interface
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
     list_filter = ('publication_year',)
+
+# book registration
 admin.site.register(Book, BookAdmin)
