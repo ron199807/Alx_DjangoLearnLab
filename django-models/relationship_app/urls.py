b348780880a0a92c5aa80540b86bf86bd6bd18be
ron199807 <ronaldmweema@gmail.com>
2024-08-22 05:16:38 +0200

    implemented user authenticatio

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),  
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # class-based view URL
-    
-    path('login/', CustomLoginView.as_view(), name='login'),  # Login URL
-    path('logout/', CustomLogoutView.as_view(), name='logout'),  # Logout URL
-    path('register/', register, name='register'),  # Registration URL
+
+        path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+
 
 ]
