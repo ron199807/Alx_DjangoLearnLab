2b44ea376d1f3e235dfe08895b1da9cfa7ff3b18
ron199807 <ronaldmweema@gmail.com>
2024-08-22 05:06:09 +0200

    implemented user authenticatio

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -4,4 +4,9 @@ from .views import list_books, LibraryDetailView
 urlpatterns = [
     path('books/', list_books, name='list_books'),  
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # class-based view URL
+    
+    path('login/', CustomLoginView.as_view(), name='login'),  # Login URL
+    path('logout/', CustomLogoutView.as_view(), name='logout'),  # Logout URL
+    path('register/', register, name='register'),  # Registration URL
+
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Book
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # Function based view to list all books
 def list_books(request):
@@ -14,3 +17,25 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library
+
+
+
+    # Custom registration view
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('list_books')  # Redirect to a view after registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+# Use Django's built-in LoginView
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+
+# Use Django's built-in LogoutView
+class CustomLogoutView(LogoutView):
+    template_name = 'logout.html'
