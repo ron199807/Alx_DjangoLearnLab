7d69525f8600c08a7961da49c258edef931e99d7
ron199807 <ronaldmweema@gmail.com>
2024-08-23 23:49:34 +0200

    added a meta class in models.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,16 +7,14 @@ urlpatterns = [
     path('books/', list_books, name='list_books'),  
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # class-based view URL
 
-        path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', views.register, name='register'),
-        path('admin/', views.admin_view, name='admin_view'),
+    path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
-
 
     path('add/', views.add_book, name='add_book'),
     path('edit/<int:book_id>', views.edit_book, name='edit_book'),
     path('delete/<int:book_id>/', views.delete_book, name='delete_book'),
-
 ]
