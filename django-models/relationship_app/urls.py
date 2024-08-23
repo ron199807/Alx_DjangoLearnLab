54203e3950511ce915d7df100838323f6e35438f
ron199807 <ronaldmweema@gmail.com>
2024-08-23 23:52:19 +0200

    added a meta class in models.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
 
-    path('add/', views.add_book, name='add_book'),
-    path('edit/<int:book_id>', views.edit_book, name='edit_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:book_id>', views.edit_book, name='edit_book'),
     path('delete/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
