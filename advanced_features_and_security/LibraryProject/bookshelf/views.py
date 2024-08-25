c628734ae69433cc5967878fe13730142ff62a6b
ron199807 <ronaldmweema@gmail.com>
2024-08-25 03:08:48 +0200

    fixed typo in settings.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 from .models import Book
+from .forms import ExampleForm
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
