f1250b1e0363912d7edd13c8f26832aaf8e68b87
ron199807 <ronaldmweema@gmail.com>
2024-08-25 03:06:29 +0200

    fixed typo in settings.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,10 @@
-<form method="post">
-    {% csrf_token %}
-    <button type="submit">Submit</button>
-</form>
+from django import forms
+
+class ExampleForm(forms.Form):
+    nama = forms.CharField(label='name', max_length=100)
+    email = forms.EmailField(label="email")
+    massage = forms.CharField(widget=forms.Textarea, label="Your massage")
+
+
+class BookSearchForm(forms.Form):
+    query = forms.CharField(max_length=100, required=False)
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -6,3 +6,11 @@ from .models import Book
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'book': books}
+
+    def book_search(request):
+        query = request.Get.get('q')
+        if query:
+            books = Book.objects.filter(title_icontains=query)
+        else:
+            books = Book.objects.all()
+            return render(request, 'bookshelf/book_list.html', {'books': books})
