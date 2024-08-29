1e12d650268b823d71e1f2d8bccf522d870a12ad
ron199807 <ronaldmweema@gmail.com>
2024-08-30 00:14:40 +0200

    added my first api project

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics.ListAPIView
 from .models import MyModel
 from .serializers import MyModelserializer
+from .serializers import BookSerializer
 
 class BookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
