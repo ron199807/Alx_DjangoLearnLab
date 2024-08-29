8a8f7789f847b612e1c740fb9af09bbf8b28a4a2
ron199807 <ronaldmweema@gmail.com>
2024-08-30 00:42:07 +0200

    added my first api project

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
-from . import views  # Import the views from the current app
+from django.urls import path, include
+from .views import BookViewSet 
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
 
 urlpatterns = [
     path('api.views.py/', views.BookListCreateAPIView.as_view(), name="book_list_create"),
