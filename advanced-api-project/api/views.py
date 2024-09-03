d9431425e8d392aa279438b50e79a682dc251c8d
ron199807 <ronaldmweema@gmail.com>
2024-09-03 17:18:19 +0200

    added views to the project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # ListView: Retrieve all books
 class BookListView(generics.ListAPIView):
