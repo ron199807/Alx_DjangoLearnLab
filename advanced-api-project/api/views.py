d2e0b2589f1de8e86305969ffc0ee89d4580709f
ron199807 <ronaldmweema@gmail.com>
2024-09-03 17:44:28 +0200

    added views to the project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -7,6 +7,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework
 
 # ListView: Retrieve all books
 class BookListView(generics.ListAPIView):
