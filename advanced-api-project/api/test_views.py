901c1ea4bd714dd96d2f39eddee30d1d5738f7f6
ron199807 <ronaldmweema@gmail.com>
2024-09-06 08:22:35 +0200

    added unit test to the project

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -3,15 +3,15 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from .models import Author, Book
 from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token
 
 class BookAPITests(APITestCase):
 
     def setUp(self):
-        # Create a user and authenticate for tests requiring authentication
+        # Create a user
         self.user = User.objects.create_user(username='testuser', password='password')
-        self.token = Token.objects.create(user=self.user)
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        
+        # Log in using session-based authentication
+        self.client.login(username='testuser', password='password')
 
         # Set up initial data
         self.author = Author.objects.create(name='Test Author')
