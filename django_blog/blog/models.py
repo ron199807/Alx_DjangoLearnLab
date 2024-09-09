c67cf6e5c17f365941a65d72e6ed10bdf3f5e02a
ron199807 <ronaldmweema@gmail.com>
2024-09-09 08:45:55 +0200

    pushing my project models.py

---
+++ django_blog/blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User # default user
+
+class post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return self.title
+
+
