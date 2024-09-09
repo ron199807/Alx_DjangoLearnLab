90382653d0969d5a050b85627902d992a3af398d
ron199807 <ronaldmweema@gmail.com>
2024-09-09 09:23:23 +0200

    added unit test to the project

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog_db',
+        'USER': 'root',
+        'PASSWORD': 'chisekele07',
+        'PORT': '3306',
+
     }
 }
 
