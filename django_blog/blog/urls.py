1df065c679a990f813bbcf7bdcfb158228c6b584
ron199807 <ronaldmweema@gmail.com>
2024-09-22 17:05:29 +0200

    reconfigered my templates

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import register, profile
+from .views import CustomLoginView, CustomLogoutView, register, profile
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('register/', register, name='register'),
     path('profile/', profile, name='profile'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import login
 from .forms import CustomUserCreationForm
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserChangeForm
+
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
@@ -18,3 +21,33 @@ def register(request):
 def profile(request):
     return render(request, 'registration/profile.html')
     
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+
+class CustomLogoutView(LogoutView):
+    next_page = 'home'  # Redirect to home page after logout
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def profile(request):
+    return render(request, 'profile.html')
+
+@login_required
+def profile_edit(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserChangeForm(instance=request.user)
+    return render(request, 'profile_edit.html', {'form': form})
