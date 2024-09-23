c010868e91f15ae706309daebe17c5c18e9af476
ron199807 <ronaldmweema@gmail.com>
2024-09-24 00:26:56 +0200

    added more tamplates that can handle CRUD OPERATIONS

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -9,18 +9,18 @@ from .models import Post
 
 class PostListView(ListView):
     model = Post
-    template_name = 'post_list.html'
+    template_name = 'blog/post_list.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']  # Display posts in reverse chronological order
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'post_detail.html'
+    template_name = 'blog/post_detail.html'
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
-    template_name = 'post_form.html'
+    template_name = 'blog/post_form.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -29,7 +29,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
-    template_name = 'post_form.html'
+    template_name = 'blog/post_form.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -42,7 +42,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     success_url = '/'
-    template_name = 'post_confirm_delete.html'
+    template_name = 'blog/post_confirm_delete.html'
 
     def test_func(self):
         post = self.get_object()
