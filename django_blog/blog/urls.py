f7fd4d0ced65a1a51db452b0cacc01baac0095d9
ron199807 <ronaldmweema@gmail.com>
2024-09-24 00:31:21 +0200

    added more tamplates that can handle CRUD OPERATIONS

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,6 +17,8 @@ path('login/', CustomLoginView.as_view(), name='login'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+
 ]
 
 
