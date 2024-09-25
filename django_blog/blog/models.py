7f4746b08d17617b6c9e6d01b9df139bd1839cdb
ron199807 <ronaldmweema@gmail.com>
2024-09-25 14:54:10 +0200

    fixed taggableManager bug

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils import timezone
+from taggit.managers import TaggableManager
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
