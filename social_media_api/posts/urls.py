49bf3b4b61cc4582889274ddfb80e9ba85812602
ron199807 <ronaldmweema@gmail.com>
2024-10-04 12:57:54 +0200

    following and unfollowing functionality

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,8 +1,19 @@
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
+from django.urls import path
+from .views import FollowedUsersPostsView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
 
-urlpatterns = router.urls
+# urlpatterns =  router.urls
+
+
+urlpatterns = [
+    router.urls
+
+    # URL for the feed of posts from followed users
+    path('feed/', FollowedUsersPostsView.as_view(), name='followed-users-feed'),
+]
+
