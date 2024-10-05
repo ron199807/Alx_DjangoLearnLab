601f7f44719dd0c7673572415bfbab2f1442baf0
ron199807 <ronaldmweema@gmail.com>
2024-10-05 15:07:57 +0200

    added like and unlike functionality

---
+++ social_media_api/posts/notification/models.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='notifications')
+    actor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='actor')
+    verb = models.CharField(max_length=255)  # Description of the action (e.g., "liked", "commented")
+    target_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    target_object_id = models.PositiveIntegerField()
+    target = GenericForeignKey('target_content_type', 'target_object_id')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    unread = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['-timestamp']
